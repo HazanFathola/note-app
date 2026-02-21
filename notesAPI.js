46a900407023663b55c508d2ae644e823068da64
Hassan Mohammed <hazan.fathola@gmail.com>
2026-02-21 13:54:06 +0100

    feat: add save note logic and localStorage integration

--- notesAPI.js
+++ notesAPI.js
@@ -1,34 +1,17 @@
 const LOCAL_STORAGE_KEY = "noteapp-notice";
 
-const MOCK_NOTES = [
-  {
-    id: "1",
-    title: "Notiz 1",
-    content: "Lorem Ipsum dolor sit amet",
-    lastUpdated: 1771588590158,
-  },
-  {
-    id: "2",
-    title: "Notiz 2",
-    content: "Lorem Ipsum dolor sit amet",
-    lastUpdated: 1771588610258,
-  },
-  {
-    id: "3",
-    title: "Notiz 3",
-    content: "Lorem Ipsum dolor sit amet",
-    lastUpdated: 1771588620358,
-  },
-  {
-    id: "4",
-    title: "Notiz 4",
-    content: "Lorem Ipsum dolor sit amet",
-    lastUpdated: 1771588630458,
-  },
-  {
-    id: "5",
-    title: "Notiz 5",
-    content: "Lorem Ipsum dolor sit amet",
-    lastUpdated: 1771588690558,
-  },
-];
+function getNotes() {
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+}
+
+function saveNote(newTitle, newContent) {
+  const notes = getNotes();
+  notes.push({
+    id: "",
+    title: newTitle,
+    content: newContent,
+    lastUpdated: new Date().getTime(),
+  });
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
+}
--- script.js
+++ script.js
@@ -5,45 +5,39 @@ const contentInputEl = document.getElementById("content-input");
 
 saveBtnEl.addEventListener("click", clickSaveButton);
 
+displayNotesList();
+
 function displayNotesList() {
-  const notes = MOCK_NOTES;
+  const notes = getNotes();
+  if (notes) {
+    const sortedNotes = [...notes].sort(
+      (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated,
+    );
 
-  const sortedNotes = [...notes].sort(
-    (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated,
-  );
-  let html = "";
+    let html = "";
 
-  sortedNotes.forEach((note) => {
-    html += `
+    sortedNotes.forEach((note) => {
+      html += `
             <div class="note-entry" data-id="${note.id}">
               <div class="note-title">${note.title}</div>
               <div class="note-content-teaser">${note.content}</div>
               <div class="note-date">${new Date(note.lastUpdated).toLocaleString("de-DE")}</div>
             </div>
             `;
-  });
-
-  notesListEl.innerHTML = html;
+    });
+    notesListEl.innerHTML = html;
+  }
 }
-
 function clickSaveButton() {
-  const newNote = {
-    id: "",
-    title: "",
-    content: "",
-    lastUpdated: new Date(),
-  };
   const titleInput = document.getElementById("title-input");
   const contentInput = document.getElementById("content-input");
 
   const title = titleInput.value.trim();
   const content = contentInput.value.trim();
-  if (title !== "" && content !== "") {
-    newNote.title = titleInput.value;
-    newNote.content = contentInput.value;
-    MOCK_NOTES.push(newNote);
-    displayNotesList();
-  } else {
+  if (title === "" && content === "") {
     alert("Bitte Titel und Inhalt eingeben");
+  } else {
+    saveNote(title, content);
+    displayNotesList();
   }
 }
