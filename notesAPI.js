0fcd2d938ba0c9c63026a8f174da6abbdf570c88
Hassan Mohammed <hazan.fathola@gmail.com>
2026-02-24 10:50:29 +0100

    add nextId generation logic

--- notesAPI.js
+++ notesAPI.js
@@ -7,11 +7,28 @@ function getNotes() {
 function saveNote(newTitle, newContent) {
   const notes = getNotes();
   notes.push({
-    id: "",
+    id: genNextId(),
     title: newTitle,
     content: newContent,
     lastUpdated: new Date().getTime(),
   });
 
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
+  console.log(notes);
+}
+
+function genNextId() {
+  const notes = getNotes();
+
+  const sortedNotes = [...notes].sort((noteA, noteB) => noteA.id - noteB.id);
+
+  let nextId = 1;
+
+  for (let note of sortedNotes) {
+    if (nextId < note.id) break;
+
+    nextId = note.id + 1;
+  }
+
+  return nextId;
 }
--- script.js
+++ script.js
@@ -9,24 +9,23 @@ displayNotesList();
 
 function displayNotesList() {
   const notes = getNotes();
-  if (notes) {
-    const sortedNotes = [...notes].sort(
-      (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated,
-    );
 
-    let html = "";
+  const sortedNotes = [...notes].sort(
+    (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated,
+  );
 
-    sortedNotes.forEach((note) => {
-      html += `
+  let html = "";
+
+  sortedNotes.forEach((note) => {
+    html += `
             <div class="note-entry" data-id="${note.id}">
               <div class="note-title">${note.title}</div>
               <div class="note-content-teaser">${note.content}</div>
               <div class="note-date">${new Date(note.lastUpdated).toLocaleString("de-DE")}</div>
             </div>
             `;
-    });
-    notesListEl.innerHTML = html;
-  }
+  });
+  notesListEl.innerHTML = html;
 }
 function clickSaveButton() {
   const titleInput = document.getElementById("title-input");
@@ -34,10 +33,14 @@ function clickSaveButton() {
 
   const title = titleInput.value.trim();
   const content = contentInput.value.trim();
-  if (title === "" && content === "") {
+  if (!title || !content) {
     alert("Bitte Titel und Inhalt eingeben");
   } else {
     saveNote(title, content);
+
+    titleInput.value = "";
+    contentInput.value = "";
+
     displayNotesList();
   }
 }
